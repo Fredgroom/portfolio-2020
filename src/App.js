6d699bccbc7c1b416494a3fabfa3380c24d6c2c5
Freddie Groom <fredgroom@gmail.com>
2019-12-30 22:09:54 +0000

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.scss";
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h3>Freddie Groom - Software Development</h3>
       </header>
+      <body>
+        <p>I have been learning to code since April 2017.</p>
+        <p>
+          I Initially learnt as much as I could from Free Code Camp, Codecademy
+          and Udemy but eventually enrolled and completed Red Academy's Full
+          Stack developer course at the end of 2018
+        </p>
+        <p>I am a currently an associate developer at Decision Technology</p>
+        <p>My current responsibilities involve</p>
+        <ul>
+          <li></li>
+        </ul>
+      </body>
+      <footer>
+        <p>email - freddie@freddiegroom.com</p>
+        <p>telephone - 07760556339</p>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
